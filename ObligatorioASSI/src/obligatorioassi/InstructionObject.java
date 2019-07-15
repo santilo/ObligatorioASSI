ccd86174d11ad5539b9cd41ed630997834c6a082
Juani <example>
2019-07-14 22:03:13 -0300

    asd

--- ObligatorioASSI/src/obligatorioassi/InstructionObject.java
+++ ObligatorioASSI/src/obligatorioassi/InstructionObject.java
@@ -7,34 +7,35 @@ public class InstructionObject {
 	private int valor;
 
 	public InstructionObject(String instr) {
-		String[] instruccion = instr.split(" ");
-		// Reviso si la instruccion es del largo apropiado
-		if ((instruccion.length != 4 && instruccion[0].toUpperCase().equals("WRITE")) || (instruccion.length != 3 && instruccion[0].toUpperCase().equals("READ"))) {
-			this.instruccion = "BAD";
-			// Reviso si la primer palabra es read/write
-		} else if (instruccion[0].toUpperCase().equals("READ") == false && instruccion[0].toUpperCase().equals("WRITE") == false) {
-			this.instruccion = "BAD";
-			// Reviso si sujeto existe
-		} else if (!SecureSystem.obtenerSujetos().containsKey(instruccion[1].toLowerCase())) {
-			this.instruccion = "BAD";
-			// Reviso si obj existe
-		} else if (!ObjectManager.obtenerObjetos().containsKey(instruccion[2].toLowerCase())) {
-			this.instruccion = "BAD";
-			// Reviso si la 4ta palabra es un entero
-		} else if (instruccion[0].toUpperCase().equals("WRITE") && !esNumerico(instruccion[3])) {
-			this.instruccion = "BAD";
-		} else if (instruccion[0].toUpperCase().equals("READ")) {
-			this.instruccion = "READ";
-			this.sujeto = instruccion[1].toLowerCase();
-			this.objeto = instruccion[2].toLowerCase();
-			ReferenceMonitor.Leer(obtenerInstructionObject());
-		} else if (instruccion[0].toUpperCase().equals("WRITE")) {
-			this.instruccion = "WRITE";
-			this.sujeto = instruccion[1].toLowerCase();
-			this.objeto = instruccion[2].toLowerCase();
-			this.valor = Integer.parseInt(instruccion[3]);
-			ReferenceMonitor.Escribir(obtenerInstructionObject());
-		}
+            this.instruccion	= " ";
+            String[] instruccion = instr.split(" ");
+            // Reviso si la instruccion es del largo apropiado acorde a la operacion a ejecutar
+            if ((instruccion.length != 4 && instruccion[0].toUpperCase().equals("WRITE")) || (instruccion.length != 3 && instruccion[0].toUpperCase().equals("READ"))) {
+                this.instruccion = "BAD";
+                // Reviso si la primer palabra es read/write
+            } else if (!instruccion[0].toUpperCase().equals("READ") && !instruccion[0].toUpperCase().equals("WRITE")) {
+                this.instruccion = "BAD";
+                // Reviso si el sujeto y objeto existen
+            } else if (!SecureSystem.obtenerSujetos().containsKey(instruccion[1].toLowerCase()) || !ObjectManager.obtenerObjetos().containsKey(instruccion[2].toLowerCase()) ) {
+                this.instruccion = "BAD";
+                //Si voy a escribir, el valor tiene que ser numerico
+            } else if (instruccion[0].toUpperCase().equals("WRITE") && !esNumerico(instruccion[3])) {
+                this.instruccion = "BAD";
+            }
+            if(this.instruccion != "BAD"){
+                if (instruccion[0].toUpperCase().equals("READ")) {
+                    this.instruccion = "READ";
+                    this.sujeto = instruccion[1].toLowerCase();
+                    this.objeto = instruccion[2].toLowerCase();
+                    ReferenceMonitor.Leer(obtenerInstructionObject());
+                } else if (instruccion[0].toUpperCase().equals("WRITE")) {
+                    this.instruccion = "WRITE";
+                    this.sujeto = instruccion[1].toLowerCase();
+                    this.objeto = instruccion[2].toLowerCase();
+                    this.valor = Integer.parseInt(instruccion[3]);
+                    ReferenceMonitor.Escribir(obtenerInstructionObject());
+                }
+            }
 	}
          
         
