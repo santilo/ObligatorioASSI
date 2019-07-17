a0b15278b297012583b1becb7cfbfce4d16ae5e9
Juani <example>
2019-07-16 21:08:01 -0300

    tesasdasd

--- ObligatorioASSI/src/obligatorioassi/InstructionObject.java
+++ ObligatorioASSI/src/obligatorioassi/InstructionObject.java
@@ -9,7 +9,8 @@ public class InstructionObject {
 	public InstructionObject(String instr) {
             this.instruccion	= " ";
             String[] linea = instr.split(" ");
-           
+            linea[1] = linea[1].toLowerCase();
+            linea[2] = linea[2].toLowerCase();
             //si llega instruccion de Run, creo una nueva instancia de InstrucionObject
              if (linea[0].toUpperCase().equals("RUN")) {
                 this.instruccion = "RUN";
@@ -17,12 +18,12 @@ public class InstructionObject {
                 ReferenceMonitor.Ejecutar(obtenerInstructionObject());
            
             //Reviso si en la primera posicion de la instruccion llega un comando valido
-            } else if (!linea[0].equals("READ") && !linea[0].equals("WRITE")&& !linea[0].equals("CREATE") && !linea[0].equals("DESTROY") && !linea[0].equals("RUN")) {
+            } else if (!linea[0].toUpperCase().equals("READ") && !linea[0].toUpperCase().equals("WRITE")&& linea[0].toUpperCase().equals("CREATE") && !linea[0].toUpperCase().equals("DESTROY") && !linea[0].toUpperCase().equals("RUN")) {
 		this.instruccion = "BAD";
            
             // Reviso si la instruccion es del largo apropiado acorde a la operacion a ejecutar
-            }else if ((linea.length != 4 && linea[0].toUpperCase().equals("WRITE")) || (linea.length != 3 && linea[0].toUpperCase().equals("READ")) || (linea.length != 3 && linea[0].equals("CREATE"))
-                || (linea.length != 3 && linea[0].equals("DESTROY")) || (linea.length != 2 && linea[0].equals("RUN"))) {
+            }else if ((linea.length != 4 && linea[0].toUpperCase().toUpperCase().equals("WRITE")) || (linea.length != 3 && linea[0].toUpperCase().toUpperCase().equals("READ")) || (linea.length != 3 && linea[0].toUpperCase().equals("CREATE"))
+                || (linea.length != 3 && linea[0].toUpperCase().equals("DESTROY")) || (linea.length != 2 && linea[0].toUpperCase().equals("RUN"))) {
                 this.instruccion = "BAD";
               
             // Reviso si el sujeto y objeto existen
@@ -34,47 +35,51 @@ public class InstructionObject {
                 this.instruccion = "BAD";
              
             //tiene que existir el objeto a eliminar    
-            } else if (linea[0].equals("DESTROY") && !ObjectManager.obtenerObjetos().containsKey(linea[2])) {
+            } else if (linea[0].toUpperCase().equals("DESTROY") && !ObjectManager.obtenerObjetos().containsKey(linea[2])) {
                    this.instruccion = "BAD";
-                   
-            //existe el objeto a crear?
-            } else if (linea[0].equals("CREATE") && ObjectManager.obtenerObjetos().containsKey(linea[2])) {
+      
+            //si ya existe el objeto a crear esta mal
+            } else if (linea[0].toUpperCase().equals("CREATE") && ObjectManager.obtenerObjetos().containsKey(linea[2])) {
                 this.instruccion = "BAD";
             }
              
             if(this.instruccion != "BAD"){
-                if (linea[0].toUpperCase().equals("READ")) {
-                    this.instruccion = "READ";
-                    this.sujeto = linea[1];
-                    this.objeto = linea[2];
-                    ReferenceMonitor.Leer(obtenerInstructionObject());
-                } else if (linea[0].toUpperCase().equals("WRITE")) {
-                    this.instruccion = "WRITE";
-                    this.sujeto = linea[1];
-                    this.objeto = linea[2];
-                    this.valor = Integer.parseInt(linea[3]);
-                    ReferenceMonitor.Escribir(obtenerInstructionObject());
-                } else if (linea[0].equals("CREATE")) {
-                    this.instruccion = "CREATE";
-                    this.sujeto = linea[1];
-                    this.objeto = linea[2];
-                    ReferenceMonitor.Crear(obtenerInstructionObject());
-		} else if (linea[0].toUpperCase().equals("DESTROY")) {
-                    this.instruccion = "DESTROY";
-                    this.sujeto = linea[1];
-                    this.objeto = linea[2];
-                    ReferenceMonitor.Destruir(obtenerInstructionObject());
+               switch(linea[0].toUpperCase()) {
+                    case "READ":
+                        this.instruccion = "READ";
+                        this.sujeto = linea[1];
+                        this.objeto = linea[2];
+                        ReferenceMonitor.Leer(obtenerInstructionObject());
+                        break;
+                    case "WRITE":
+                        this.instruccion = "WRITE";
+                        this.sujeto = linea[1];
+                        this.objeto = linea[2];
+                        this.valor = Integer.parseInt(linea[3]);
+                        ReferenceMonitor.Escribir(obtenerInstructionObject());
+                    break;
+                    case "CREATE":
+                        this.instruccion = "CREATE";
+                        this.sujeto = linea[1];
+                        this.objeto = linea[2];
+                        ReferenceMonitor.Crear(obtenerInstructionObject());
+                    case "DESTROY":
+                        this.instruccion = "DESTROY";
+                        this.sujeto = linea[1];
+                        this.objeto = linea[2];
+                        ReferenceMonitor.Destruir(obtenerInstructionObject());  
+                        break;
                 }
             }
 	}
          
         
 	// Reviso si mi valor es un numero entero o no
-	public static boolean esNumerico(String s) {
+	public static boolean esNumerico(String valor) {
 		try {
-			Integer.parseInt(s);
+                    Integer.parseInt(valor);
 		} catch (NumberFormatException e) {
-			return false;
+                    return false;
 		}
 		return true;
 	}
